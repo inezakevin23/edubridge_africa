acf686748570b509bca344b0a32497efa85f44de
Kevin Ineza <k.ineza1@alustudent.com>
2026-07-27 00:34:48 +0200

    forced absolute container static roots to clear MIME 404 blocks

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -164,7 +164,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = '/app/staticfiles'
 STORAGES = {
     "default": {
         "BACKEND": "django.core.files.storage.FileSystemStorage",

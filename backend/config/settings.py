690d5d76c62598d020bc56e5ce9f0390a397330c
Kevin Ineza <k.ineza1@alustudent.com>
2026-07-28 14:01:55 +0200

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -162,7 +162,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
-BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
